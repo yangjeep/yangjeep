1f992d84a321e703ea150ea66321bcc2d63fe174
Jiajian Yang <jiajian.yang@searchspring.com>
2022-10-28 15:37:33 -0400

    add files

---
+++ rc/brew.sh
@@ -0,0 +1,43 @@
+# install brews
+brew install git
+brew install python
+brew install python2
+brew install tmux
+brew install thefuck
+brew install telnet
+
+# install brew cask
+brew tap caskroom/cask
+
+# cask-drivers
+brew tap homebrew/cask-drivers
+
+# gotop
+brew tap cjbassi/gotop
+brew install gotop
+
+# install all the casks
+brew cask install adobe-acrobat-reader 
+brew cask install aliwangwang 
+brew cask install amazon-music 
+brew cask install amazon-photos 
+brew cask install appcleaner
+brew cask install atom
+brew cask install bartender
+brew cask install cloudapp
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install iina
+brew cask install impactor
+brew cask install istat-menus
+brew cask install iterm2
+brew cask install logitech-options
+brew cask install middleclick
+brew cask install neteasemusic
+brew cask install sizeup
+brew cask install skype
+brew cask install sogouinput
+brew cask install visual-studio-code
+brew cask install vnc-viewer
+brew cask install xmind
+
